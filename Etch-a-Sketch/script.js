c21b57dbed77e29bfaa408ede62fd0115be7428e
joshuaadu <joshuaadu2001@gmail.com>
2020-06-21 00:16:13 +0000

    Added an external js file; Work on Buttons

---
+++ Etch-a-Sketch/script.js
@@ -0,0 +1,9 @@
+const create = document.querySelector('#create');
+create.addEventListener('click', () => {
+    alert("It is working!");
+});
+
+const clear = document.querySelector('#clear');
+clear.addEventListener('click', () => {
+    alert("It is working!");
+});
