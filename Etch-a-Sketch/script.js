4d8745a271ddda427efa2263f15e4211be8e9ca8
joshuaadu <joshuaadu2001@gmail.com>
2020-06-22 18:42:04 +0000

    Cleaned Up Code

--- Etch-a-Sketch/script.js
+++ Etch-a-Sketch/script.js
@@ -1,28 +1,16 @@
 const sketchBoard = document.querySelector(".sketchBoard");
-/*
-const create = document.querySelector("#create");
-create.addEventListener("click", createGrid);
-
-const clear = document.querySelector("#clear");
-clear.addEventListener("click", clearGrid);
-
-const test = document.querySelector("#test");
-test.addEventListener("click", () => {
-  alert("Hello World");
-});
-*/
 
 function createGrid(gridSize) {
   const div = document.createElement("div");
   div.className = "sketchDivs";
-  //Loop to create columns
+  
   for (i = 0; i < 10; i++) {
-    //Loop to create rows
+   
 
     sketchBoard.appendChild(div);
     console.log(i);
   }
-  //   alert(`Grid Size is now ${gridSize}`);
+  
 }
 
 function clearGrid() {
