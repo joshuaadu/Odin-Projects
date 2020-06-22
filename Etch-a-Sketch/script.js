6423a8161560f0ea1bf662b8d9a1f04c7e481bfd
joshuaadu <joshuaadu2001@gmail.com>
2020-06-22 18:37:37 +0000

    Added Event Listeners to buttons

--- Etch-a-Sketch/script.js
+++ Etch-a-Sketch/script.js
@@ -1,9 +1,43 @@
-const create = document.querySelector('#create');
-create.addEventListener('click', () => {
-    alert("It is working!");
-});
+const sketchBoard = document.querySelector(".sketchBoard");
+/*
+const create = document.querySelector("#create");
+create.addEventListener("click", createGrid);
 
-const clear = document.querySelector('#clear');
-clear.addEventListener('click', () => {
-    alert("It is working!");
+const clear = document.querySelector("#clear");
+clear.addEventListener("click", clearGrid);
+
+const test = document.querySelector("#test");
+test.addEventListener("click", () => {
+  alert("Hello World");
+});
+*/
+
+function createGrid(gridSize) {
+  const div = document.createElement("div");
+  div.className = "sketchDivs";
+  //Loop to create columns
+  for (i = 0; i < 10; i++) {
+    //Loop to create rows
+
+    sketchBoard.appendChild(div);
+    console.log(i);
+  }
+  //   alert(`Grid Size is now ${gridSize}`);
+}
+
+function clearGrid() {
+  alert(`Grid Size is now ${gridSize - gridSize}`);
+}
+
+//Add event listeners to buttons using the target property
+const buttons = document.querySelector(".container");
+buttons.addEventListener("click", (event) => {
+  if (event.target.textContent == "Create") {
+    const gridSize = Number(prompt());
+    createGrid(gridSize);
+  } else if (event.target.textContent == "Clear") {
+    clearGrid(gridSize);
+  } else {
+    console.log("default");
+  }
 });
