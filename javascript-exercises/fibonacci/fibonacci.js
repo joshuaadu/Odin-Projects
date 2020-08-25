474e284b9e566f572dd9cc1d1813edffa4be9492
joshuaadu <joshuaadu2001@gmail.com>
2020-08-25 22:25:44 +0000

    Completed fibonacci and getTheTitles exercises

--- javascript-exercises/fibonacci/fibonacci.js
+++ javascript-exercises/fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    num = Number(num)
+    const sequence = [1,1]; 
+    if(num < 0){
+        return "OOPS";
+    }
+    for (let i = 2; i <num; i++){
+        sequence.push(sequence[i - 2] + sequence[i -1]);
+    }
+    return sequence[num-1];
 }
 
 module.exports = fibonacci
--- javascript-exercises/getTheTitles/getTheTitles.js
+++ javascript-exercises/getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function (books) {
+    const bookTitles = books.reduce((list, book) => {
+        list.push(book.title);
+        return list;
+    }, [])
+    return bookTitles;
 }
 
 module.exports = getTheTitles;
