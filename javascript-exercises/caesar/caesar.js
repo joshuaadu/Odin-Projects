b473846e07e9e981da344d050d03d0c96dcb4fc7
joshuaadu <joshuaadu2001@gmail.com>
2020-08-29 15:35:23 +0000

    Completed caesar and findTheOldest exercises

--- javascript-exercises/caesar/caesar.js
+++ javascript-exercises/caesar/caesar.js
@@ -1,5 +1,53 @@
-const caesar = function() {
+const caesar = function (str, shift) {
+  const nstr = str.toUpperCase();
+  let results = [];
+  let lowerCaseIndexes = [];
+  //Loop through string
+  for (let i = 0; i < str.length; i++) {
+    let char;
+    //Check and change each character to UpperCase for shifting
+    if (isLowerCase(str[i])) {
+      char = str[i].toUpperCase();
+      //Track index of LowerCase character
+      lowerCaseIndexes.push(i);
+    } else {
+      char = str[i];
+    }
+    //Get ASCII code
+    strCode = nstr.charCodeAt(i);
+    //Shift only alphabets
+    if (strCode >= 65 && strCode <= 90) {
+      //Handle large positve or negative shifts
+      if (shift > 25 || shift < -25) {
+        shift = shift % 26;
+      }
 
+      if (strCode + shift > 90) {
+        let diff = strCode + shift - 90;
+        console.log(diff);
+        results.push(String.fromCharCode(diff + 64));
+      } else if (strCode + shift < 65) {
+        let diff = 65 - (strCode + shift);
+        console.log(diff);
+        results.push(String.fromCharCode(91 - diff));
+      } else {
+        results.push(String.fromCharCode(strCode + shift));
+      }
+    } else {
+      results.push(String.fromCharCode(strCode));
+    }
+  }
+  //Change characters to their original letter case
+  lowerCaseIndexes.forEach((index) => {
+    results[index] = results[index].toLowerCase();
+  });
+
+  return results.join("");
+};
+
+//Check if character is lowerCase
+function isLowerCase(chr) {
+  return chr == chr.toLowerCase();
 }
 
-module.exports = caesar
+module.exports = caesar;
--- javascript-exercises/findTheOldest/findTheOldest.js
+++ javascript-exercises/findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-let findTheOldest = function() {
+let findTheOldest = function (people) {
+  return people.reduce((oldest, currentPerson) => {
+    const olderAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+    const currentAge = getAge(
+      currentPerson.yearOfBirth,
+      currentPerson.yearOfDeath
+    );
+    return olderAge < currentAge ? currentPerson : oldest;
+  });
+};
 
-}
+const getAge = function (birth, death) {
+  if (!death) {
+    death = new Date().getFullYear();
+  }
+  return death - birth;
+};
 
-module.exports = findTheOldest
+module.exports = findTheOldest;
