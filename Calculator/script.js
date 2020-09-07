fe3e24909eacb4aebb843101ee1a6a0cdc919609
joshuaadu <joshuaadu2001@gmail.com>
2020-09-07 19:47:23 +0000

    Brother Sunday

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,33 +1,46 @@
+const buttons = document.querySelector(".buttons");
+//buttons.addEventListener("click", () => {
+//alert("it works");
+//});
 
-const operate = function(operator, operand1, operand2){
-    switch (operator){
-        case "+": 
-            add(operand1, operand2);
-            break;
+const btnKeys = document.querySelectorAll('.btnKeys');
+btnKeys.forEach((index) => index.addEventListener("click", () => {
+    popDisplay(index);
+}));
+
+const operate = function (operator, operand1, operand2) {
+    switch (operator) {
+        case "+":
+            return add(operand1, operand2);
         case "-":
-            substract(operand1, operand2);
-            break;
+            return substract(operand1, operand2);
         case "*":
-            multiply(operand1, operand2);
-            break;
+            return multiply(operand1, operand2);
+
         case "/":
-            divide(operand1, operand2);
-            break;
+            return divide(operand1, operand2);
+        default:
+            return 0;
     }
 }
 
-const add = function(a, b){
+const add = function (a, b) {
     return a + b;
 }
 
-const substract = function(a, b){
+const substract = function (a, b) {
     return a - b;
 }
 
-const multiply = function(a, b){
+const multiply = function (a, b) {
     return a * b;
 }
 
-const divide = function(a, b){
+const divide = function (a, b) {
     return a / b;
+}
+
+const popDisplay = function (e) {
+    const display = document.querySelector("#calcExp");
+    display.value += e.textContent;
 }
